8955df2e516c5d55c04c034031dc644b56a4a7be
Jameslzhang <48320150+Jameslzhang@users.noreply.github.com>
2023-05-15 21:51:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'hYdIty3QUWsWgnoYNlXwq91sv7AG3UL3Ak8_RGDIRiA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
