5032877b4cf93408ffcf63d5af63b035fcfe36ae
Jameslzhang <48320150+Jameslzhang@users.noreply.github.com>
2023-09-23 22:25:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooyph5yaY_9SQgajLBCIdoDdPjmY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'BPM4aeYu2ec9-6LzSvCQfj1ZrEWryQ0r3rRPJQDTv3g',
+      useTemplateId: 'K3fRBGWduTVRD8Tp29Ub8Yu76ui54Rd39qJKxK8W3DA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-31',
       festivals: [
@@ -36,13 +36,13 @@ const USER_CONFIG = {
           type: '生日', name: '宝贝', year: '1999', date: '03-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '12-09',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-12-09' },
+        { keyword: 'love_day', date: '2022-12-19' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'BPM4aeYu2ec9-6LzSvCQfj1ZrEWryQ0r3rRPJQDTv3g',
+  CALLBACK_TEMPLATE_ID: 'ZWpr8xpsHDjS_afxAh8cpo_nUX-taGSAwP8SexC2V14',
 
   CALLBACK_USERS: [
     {
