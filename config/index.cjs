146ab38a1c2b0fc260242fbe35e549676cd2cdb3
Jameslzhang <48320150+Jameslzhang@users.noreply.github.com>
2023-10-12 23:31:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,37 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-12-19' },
+        { keyword: 'love_day', date: '2022-12-09' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: 'kk宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ooyph51cWxsRKJYnLkGbXxLtjPIc',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '53IEx8oOZRo4RrmYvH8jEbOzYIFx3jSXvPxVlXZigpI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-31',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1999', date: '02-14',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1999', date: '03-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-19',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-12-09' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
